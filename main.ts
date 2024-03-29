bbbddd5f8dc0e83c13a34501f2a0fcb14f18f2d5
maryumxx <162018235+maryumxx@users.noreply.github.com>
2024-03-30 01:17:56 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+import inquirer from 'inquirer';
+
+const randomNumber = Math.floor(Math.random() * 6 + 1)
+
+
+const answer = await inquirer.prompt([
+    {
+        message: 'Guess a number between 1-6: ',
+        name: 'userGuessedNumber',
+        type: 'number',
+    },
+])
+
+if (answer.userGuessedNumber === randomNumber){
+    console.log('Congratulations! You guessed it right.')
+}
+else
+    console.log('Guessed it wrong! Try Again.')
